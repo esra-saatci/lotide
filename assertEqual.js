8daec2f38ab1c1fa0e93257e1e43076e152fc112
esra-saatci <esra.saatci86@gmail.com>
2021-07-20 21:54:10 +0000

    Git push

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@ let actual = "Lighthouse Labs";
 let expected = "Bootcamp";
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("Assertion Failed: " + actual + " !== " + expected);
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
