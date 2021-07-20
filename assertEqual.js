9f013cd7ae44c9fc629a3feb6f06514c9a84f159
esra-saatci <esra.saatci86@gmail.com>
2021-07-20 02:23:44 +0000

    Implementing assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+let actual = "Lighthouse Labs";
+let expected = "Bootcamp";
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
