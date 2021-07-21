79f9cbf403927fe34c9a129e7a8a3d11f7ce440e
esra-saatci <esra.saatci86@gmail.com>
2021-07-21 23:44:26 +0000

    Counting the letters of a word

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const results = {};
+  for (let letter of sentence) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+assertEqual(countLetters("lighthouse")["l"], 1);
