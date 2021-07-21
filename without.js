f7fa0476dab6024cdb928c0955c8a72b1ce5d213
esra-saatci <esra.saatci86@gmail.com>
2021-07-21 03:03:18 +0000

    IMplement without function to remove some unwanted items

---
+++ without.js
@@ -0,0 +1,13 @@
+const without = function(source, itemsToRemove) {
+  let withoutArray = [];
+
+  for (let i=0; i<source.length; i++) {
+    if(!itemsToRemove.includes(source[i])) {
+      withoutArray.push(source[i]);
+    }
+  }
+  return withoutArray;
+}
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
