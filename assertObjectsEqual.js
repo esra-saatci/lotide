a56a650628c8d46795000baac02edf95e64dde30
esra-saatci <esra.saatci86@gmail.com>
2021-07-22 03:53:26 +0000

    Implement assertObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,31 +1,50 @@
 const eqObjects = function(object1, object2) {
-  let obj1 = Object.keys(object1);
-  let obj2 = Object.keys(object2);
-  if (obj1.length !== obj2.length) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  }
-  for (let value of obj1) {
-    if (Array.isArray(obj1[value]) === Array.isArray(obj2[value])) {
-      return true;
+  } else {
+    let allMatch = true;
+    for (let key of Object.keys(object1)) {
+      let currentKey = key;
+      let currentMatch = false;
+      for (let otherKey of Object.keys(object2)) {
+        if (Array.isArray(object1[currentKey]) && Array.isArray(object2[otherKey]) && eqArrays(object1[currentKey], object2[otherKey])) {
+          currentMatch = true;
+        }
+        if (currentKey === otherKey && object1[currentKey] === object2[currentKey]) {
+          currentMatch = true;
+        }
+      }
+      if (currentMatch === false) {
+        allMatch = false;
+      }
     }
-    return false;
+    return allMatch;
   }
+};
+
+const eqArrays = function(actual, expected) {
+  //If array lengths do not match, false.
+  if (actual.length !== expected.length) {
+    return false;
+  //If all items are not identical, false
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  //If above conditions are not triggered, true.
   return true;
 };
 
-// FUNCTION IMPLEMENTATION
+
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertObjectsEqual(ab, ba); // => true
-  
-const abc = { a: "1", b: "2", c: "3" };
-assertObjectsEqual(ab, abc); // => false
+assertObjectsEqual({a: 1, b: 2, c: [1, 2, 3]}, {a: 1, b: 2, c: [1, 2, 3]});
