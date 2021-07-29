5697aa7108f36cbe97a384bc4c7570163ce09a2e
esra-saatci <esra.saatci86@gmail.com>
2021-07-29 03:35:28 +0000

    Edited head.js

--- head.js
+++ head.js
@@ -1,17 +1,19 @@
-let actual = "Lighthouse Labs";
-let expected = "Bootcamp";
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
 
-const head = function(_array){
+
+const head = function(_array) {
   return _array[0];
 };
 
 
-assertEqual(head(["Lighthouse Labs", "Bootcamp","Labs"]),"Lighthouse Labs");                                                                                                                                                                                                                                             
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
