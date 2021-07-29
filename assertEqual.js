07063620602a7438330e64781b221d46a232c333
esra-saatci <esra.saatci86@gmail.com>
2021-07-29 03:08:29 +0000

    Edited assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,15 @@
-let actual = "Lighthouse Labs";
-let expected = "Bootcamp";
+// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
 
 
 
+// TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1)
