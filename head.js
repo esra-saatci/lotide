bb72f7bdb1eadb511d466e950af98fbd527dc8a4
esra-saatci <esra.saatci86@gmail.com>
2021-08-12 03:40:42 +0000

    Refactored head

--- head.js
+++ head.js
@@ -1,19 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 
-const head = function(_array) {
-  return _array[0];
+const head = function(array) {
+  return array[0];
 };
 
 
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
