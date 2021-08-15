0c2599b70cb2e241c4d60337ff45fce954367dd6
esra-saatci <esra.saatci86@gmail.com>
2021-08-15 19:51:39 +0000

    Add chai test for flatten function

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -1,6 +1,7 @@
 const assert = require('chai').assert;
 const flatten = require('../flatten');
 
+
 describe("#flatten", () => {
   it("returns [1, 2, 3, 4, 5] for [1, 2, [3, 4], 5]", () => {
     assert.deepEqual(flatten([1, 2, [3, 4], 5]), [1, 2, 3, 4, 5]);
